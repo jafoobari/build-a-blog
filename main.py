5504351edef7277cd906604634c67100c67db457
Jabari A <Jabari.Allen@gmail.com>
2017-02-15 15:27:16 -0600

    Finished base HTML file and created HTML template files for frontpage and new-post pages.

--- main.py
+++ main.py
@@ -15,6 +15,14 @@
 # limitations under the License.
 #
 import webapp2
+import jinja2
+import os
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape= True)
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
